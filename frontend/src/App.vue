187c5926eb30d082ff721361a1923d9d8b9da964
almeramentok <almerageementok@gmail.com>
2026-08-08 14:45:47 +0800

    Set up Vue frontend and Express backend

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center bg-[#f5eee8]">
+    <h1 class="font-serif text-6xl font-black text-[#70453c]">
+      Portfolio Maker
+    </h1>
+  </div>
+</template>
